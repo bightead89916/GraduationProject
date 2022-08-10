bd98d2b48226c808e0dd3614f17358f65ee174c5
bightead89916 <68428529+bightead89916@users.noreply.github.com>
2022-08-10 21:14:37 +0800

    810

---
+++ html/test.js
@@ -0,0 +1,9 @@
+function OpenModal() {
+    let element = document.getElementById('overlay')
+    element.style.display = 'block'
+}
+
+function CloseModal() {
+    let element = document.getElementById('overlay')
+    element.style.display = 'none'
+}
